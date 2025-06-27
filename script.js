5af0e42b673f8b7cf0a94afe02d43edbcbfac56d
Sandro sulava <sandrosulava2@gmail.com>
2025-06-27 10:43:28 +0400

    feature

---
+++ script.js
@@ -0,0 +1,17 @@
+// todo console.log(Math.sqrt(25));
+// ! console.log(Math.pow(4, 3));
+// ? console.log(Math.max(10, 50, 1, 4, 5));
+// * console.log(Math.min(10, 5, 1, 4, 5));
+
+function sayHello() {
+  let name = "giorgi";
+  return "gamarjoba ${name}";
+}
+
+const sayHello2 = () => {
+  let name = "giorgi";
+  return "gamarjoba ${name}";
+};
+
+console.log(sayHello());
+console.log(sayHello2());
